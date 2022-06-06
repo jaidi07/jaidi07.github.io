21fe25cf27a2bb3d252a08fd3429f744b3b677da
jaidi07 <m.jaidi07@gmail.com>
2022-06-07 02:55:51 +0800

    gtw

--- js/script.js
+++ js/script.js
@@ -40,6 +40,11 @@ $.ajax({
 
 
 
+
+// const accessToken = 'IGQVJXWWNESm5SWVFqUXJacDlpQjJPR0JqaGNhUjh2QlFDNHJzZAFFFbURVOGRRWE5oLWltbUtFbDMtd1FHVmdGTXZAjWUsxVFYtYy1QNzVra3VvQzFiTVJRVnp4UmdkdWRERGh1dFFJU1VGTzNpWU81aQZDZD';
+
+
+
 // fetch('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCkXmLjEr95LVtGuIm3l2dPg&key=AIzaSyAy1VRmbcdENnppWzvnzNV4j631b78PiDU')
 //   .then(response=>console.log(response.json()))
 //   .then(response=>console.log(response))
